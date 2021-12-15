1f246f69ce68ce8f40997bd1167ce66211271717
ahsansan <93358769+ahsansan@users.noreply.github.com>
2021-12-15 16:02:20 +0700

    date and real time

--- script/blog.js
+++ script/blog.js
@@ -5,14 +5,16 @@ function addBlog(event){
 
     let title = document.getElementById('input-blog-title').value
     let content = document.getElementById('input-blog-content').value
-    let image = document.getElementById('input-blog-image').files
+    let images = document.getElementById('input-blog-image').files
 
-    image = URL.createObjectURL(image[0])
+    image = URL.createObjectURL(images[0])
 
     let blog = {
         title: title,
         content: content,
-        image: image
+        image: image,
+        author: 'Maulana Ahsan',
+        postAt: new Date()
     }
 
     blogs.push(blog)
@@ -42,14 +44,80 @@ function manipulationHTML(){
                                                 >
                                             </h1>
                                             <div class="detail-blog-content">
-                                                12 Jul 2021 22:30 WIB | Ichsan Emrald Alamsyah
+                                                ${getFullTime(blogs[i].postAt)} | ${blogs[i].author}
                                             </div>
                                             <p>
                                                 ${blogs[i].content}
                                             </p>
+                                            <div style="text-align: right; font-size: 13px; color: grey; font-style: italic;">
+                                              ${getDistanceTime(blogs[i].postAt)}
+                                            </div>
                                             </div>
                                         </div>
         
                                     `
     }
 }
+
+let month = [ 
+                'January', 
+                'February', 
+                'March', 
+                'April', 
+                'May', 
+                'June', 
+                'July', 
+                'August', 
+                'September', 
+                'October', 
+                'November', 
+                'December'
+            ]
+
+function getFullTime(time) {
+
+    let date = time.getDate()
+    let monthIndex = time.getMonth()
+    let year = time.getFullYear()
+
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+
+    let result = `${date} ${month[monthIndex]} ${year} ${hours}:${minutes} WIB`
+
+    return result
+}
+
+function getDistanceTime(time) {
+
+    let timePost = time
+    let timeNow = new Date()
+
+    let distance = timeNow - timePost
+
+    let milisecond = 1000;
+    let secondInMinutes = 60;
+    let minutesInHour = 60;
+    let hourInDay = 23;
+
+    let distanceDay = Math.floor(distance / (milisecond * secondInMinutes * minutesInHour * hourInDay))
+    let distanceHour = Math.floor(distance / (milisecond * secondInMinutes * minutesInHour))
+    let distanceMinutes = Math.floor(distance / (milisecond * secondInMinutes))
+    let distanceSecond = Math.floor(distance / milisecond)
+
+    if ( distanceDay >= 1) {
+    return `${distanceDay} day ago` }
+
+    else if ( distanceHour >= 1) {
+    return `${distanceHour} hours ago` }
+
+    else  if ( distanceMinutes >= 1) {
+    return `${distanceMinutes} minutes ago` }
+
+    else {
+        return `${distanceSecond} second ago` }
+}
+
+setInterval(()=>{
+    manipulationHTML()
+},1000)
